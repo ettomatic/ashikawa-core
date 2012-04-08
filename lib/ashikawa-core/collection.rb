26dbfe79ced7e07e9badb58e1ba0c15c8af7cca8
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-04-08 13:35:26 +0200

    Documentation added. Especially the RestApi was in need of some explanation, even though it is intended to be a helper class.

--- lib/ashikawa-core/collection.rb
+++ lib/ashikawa-core/collection.rb
@@ -1,15 +1,15 @@
 module Ashikawa
   module Core
     class Collection
-      # The name of the collection
+      # The name of the collection, must be unique.
       attr_reader :name
       
-      # The ID of the collection
+      # The ID of the collection. Is set by the database and unique.
       attr_reader :id
       
       # Create a new Collection object with a name and an optional ID
       # 
-      # @param [String] name Name of the collection
+      # @param [String] name A (unique) name for the collection
       # @param [Hash] options Options to create a collection
       # @option options [Integer] :id The ID of the collection (should not be set manually, leave that to the database)
       def initialize(name, options = {})
--- lib/ashikawa-core/rest_api.rb
+++ lib/ashikawa-core/rest_api.rb
@@ -5,11 +5,24 @@ module Ashikawa
   module Core
     class RestApi
       class << self
+        # An API connection string, for example http://localhost:8529/_api
+        # to be used for every following request as base URL.
         attr_accessor :api_string
       end
       
+      # Sends a request to a given path. Prepends the api_string automatically.
+      # Example call: 
+      ## GET request:
+      # RestApi.request('/collection/new_collection')
+      ## POST request:
+      # RestApi.request('/collection/new_collection', :post => { :name => 'new_collection' })
+      #
+      # @param [String] path the path you wish to send a request to.
+      # @param [Hash] method_params additional parameters for your request. Only needed if you want to send something other than a GET request.
+      # @option method_params [Hash] :post POST data in case you want to send a POST request.
+      # @return [Hash] parsed JSON response from the server
       def RestApi.request(path, method_params = {})
-        path.gsub! /^\//,''
+        path.gsub! /^\//, ''
         
         if method_params.has_key? :post
           JSON.parse RestClient.post("#{@api_string}/#{path}", method_params[:post])
