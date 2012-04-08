b1b17cfd5f40176539c0d9c60209c1c623d313a4
moonglum <moonglum@moonbeamlabs.com>
2012-04-08 20:55:52 +0200

    Added support for status codes and attributes #1.

--- lib/ashikawa-core/collection.rb
+++ lib/ashikawa-core/collection.rb
@@ -12,10 +12,81 @@ module Ashikawa
       # @param [Database] database The database the connection belongs to
       # @param [Hash] raw_collection The raw collection returned from the server
       def initialize(database, raw_collection)
+        @database = database
         @name = raw_collection['name'] if raw_collection.has_key? 'name'
         @id  = raw_collection['id'].to_i if raw_collection.has_key? 'id'
-        # @id = options[:id] if options.has_key? :id
+        @status = raw_collection['status'].to_i if raw_collection.has_key? 'status'
       end
+      
+      # Checks if the collection is new born. This is derived from the status code 1.
+      def new_born?
+        @status == 1
+      end
+      
+      # Checks if the collection is unloaded. This is derived from the status code 2.
+      def unloaded?
+        @status == 2
+      end
+      
+      # Checks if the collection is loaded. This is derived from the status code 3.
+      def loaded?
+        @status == 3
+      end
+      
+      # Checks if the collection is in the process of being unloaded. This is derived from the status code 4.
+      def being_unloaded?
+        @status == 4
+      end
+      
+      # Checks if the collection is deleted. This is derived from the status code 5.
+      def deleted?
+        @status == 5
+      end
+      
+      # Checks if the collection is corrupted. This is the case, if the status code is greater than 5.
+      def corrupted?
+        @status > 5
+      end
+      
+      # Checks if the collection waits for sync: If `true` then creating or changing a document will wait until the data has been synchronised to disk.
+      def wait_for_sync?
+        server_response = @database.send_request "/collection/#{id}/parameter"
+        server_response["waitForSync"]
+      end
+      
+      # Returns the number of documents in the collection.
+      # 
+      # @return [Fixnum] Number of documents
+      def length
+        server_response = @database.send_request "/collection/#{id}/count"
+        server_response["count"]
+      end
+      
+      # Return a figure for the collection. The figure can be one of:
+      # 1. :datafiles_count The number of active datafiles
+      # 2. :alive_size The total size in bytes used by all living documents
+      # 3. :alive_count The number of living documents
+      # 4. :dead_size The total size in bytes used by all dead documents
+      # 5. :dead_count The number of dead documents
+      # 
+      # @param [Symbol] figure_type The figure you want to know
+      # @return [Fixnum] The figure you requested
+      def figure(figure_type)
+        server_response = @database.send_request "/collection/#{id}/figures"
+        case figure_type
+        when :datafiles_count
+          server_response["figures"]["datafiles"]["count"]
+        when :alive_size
+          server_response["figures"]["alive"]["size"]
+        when :alive_count
+          server_response["figures"]["alive"]["count"]
+        when :dead_size
+          server_response["figures"]["dead"]["size"]
+        when :dead_count
+          server_response["figures"]["dead"]["count"]
+        end
+      end
+      
     end
   end
 end
--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -18,6 +18,82 @@ describe Ashikawa::Core::Collection do
     my_collection.id.should == 4588
   end
   
+  describe "the status code" do
+    it "should know if the collection is new born" do
+      my_collection = subject.new @database, { "status" => "1" }
+      my_collection.new_born?.should == true
+    
+      my_collection = subject.new @database, { "status" => "200" }
+      my_collection.new_born?.should == false
+    end
+  
+    it "should know if the collection is unloaded" do
+      my_collection = subject.new @database, { "status" => "2" }
+      my_collection.unloaded?.should == true
+    
+      my_collection = subject.new @database, { "status" => "200" }
+      my_collection.unloaded?.should == false
+    end
+  
+    it "should know if the collection is loaded" do
+      my_collection = subject.new @database, { "status" => "3" }
+      my_collection.loaded?.should == true
+    
+      my_collection = subject.new @database, { "status" => "200" }
+      my_collection.loaded?.should == false
+    end
+  
+    it "should know if the collection is being unloaded" do
+      my_collection = subject.new @database, { "status" => "4" }
+      my_collection.being_unloaded?.should == true
+    
+      my_collection = subject.new @database, { "status" => "200" }
+      my_collection.being_unloaded?.should == false
+    end
+    
+    it "should know if the collection is deleted" do
+      my_collection = subject.new @database, { "status" => "5" }
+      my_collection.deleted?.should == true
+    
+      my_collection = subject.new @database, { "status" => "200" }
+      my_collection.deleted?.should == false
+    end
+    
+    it "should know if the collection is corrupted" do
+      my_collection = subject.new @database, { "status" => "6" }
+      my_collection.corrupted?.should == true
+    end
+  end
+  
+  describe "attributes of a collection" do
+    it "should check if the collection waits for sync" do
+      @database.stub(:send_request).with("/collection/4590/parameter").and_return { server_response("/collections/4590") }
+      @database.should_receive(:send_request).with("/collection/4590/parameter")
+      
+      my_collection = subject.new @database, { "id" => "4590" }
+      my_collection.wait_for_sync?.should be_true
+    end
+    
+    it "should know how many documents the collection has" do
+      @database.stub(:send_request).with("/collection/4590/count").and_return { server_response("/collections/4590-parameter") }
+      @database.should_receive(:send_request).with("/collection/4590/count")
+      
+      my_collection = subject.new @database, { "id" => "4590" }
+      my_collection.length.should == 54
+    end
+    
+    it "should check for the figures" do
+      @database.stub(:send_request).with("/collection/73482/figures").and_return { server_response("/collections/73482-figures") }
+      @database.should_receive(:send_request).with("/collection/73482/figures")
+      
+      my_collection = subject.new @database, { "id" => "73482" }
+      my_collection.figure(:datafiles_count).should == 1
+      my_collection.figure(:alive_size).should == 0
+      my_collection.figure(:alive_count).should == 0
+      my_collection.figure(:dead_size).should == 2384
+      my_collection.figure(:dead_count).should == 149      
+    end
+  end
   # describe "an initialized collection" do
   #   
   # end
