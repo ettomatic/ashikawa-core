ad0b9dfd09d738fbdd477dda16864259552433e9
moonglum <moonglum@moonbeamlabs.com>
2012-04-25 16:58:46 +0200

    Added Examples for Database #10

--- lib/ashikawa-core/database.rb
+++ lib/ashikawa-core/database.rb
@@ -3,11 +3,18 @@ require "ashikawa-core/connection"
 
 module Ashikawa
   module Core
+    
+    # Represents an AvocadoDB database in Ruby
     class Database
       # Initializes the connection to the database
       # 
       # @param [Connection, String] connection A Connection object or a String to create a Connection object.
       # @api public
+      # @example Access a Database by providing the URL
+      #  database = Ashikawa::Core::Database.new "http://localhost:8529"
+      # @example Access a Database by providing a Connection
+      #  connection = Connection.new "http://localhost:8529"
+      #  database = Ashikawa::Core::Database.new connection
       def initialize(connection)
         if connection.class == String
           @connection = Ashikawa::Core::Connection.new connection
@@ -20,6 +27,9 @@ module Ashikawa
       # 
       # @return [String]
       # @api public
+      # @example Get the IP of the connection
+      #   database = Ashikawa::Core::Database.new "http://localhost:8529"
+      #   database.ip # => http://localhost
       def ip
         @connection.ip
       end
@@ -28,6 +38,9 @@ module Ashikawa
       # 
       # @return [Fixnum]
       # @api public
+      # @example Get the port for the connection
+      #   database = Ashikawa::Core::Database.new "http://localhost:8529"
+      #   database.port # => 8529
       def port
         @connection.port
       end
@@ -36,6 +49,11 @@ module Ashikawa
       # 
       # @return [Array<Collection>]
       # @api public
+      # @example Get an Array containing the Collections in the database
+      #   database = Ashikawa::Core::Database.new "http://localhost:8529"
+      #   database["a"]
+      #   database["b"]
+      #   database.collections # => [ #<Collection name="a">, #<Collection name="b">]
       def collections
         server_response = @connection.send_request "/collection"
         server_response["collections"].map { |collection| Ashikawa::Core::Collection.new self, collection }
@@ -43,8 +61,15 @@ module Ashikawa
       
       # Get or create a Collection based on name or ID
       # 
+      # @param [String, Fixnum] collection_identifier The name or ID of the collection
       # @return [Collection]
       # @api public
+      # @example Get a Collection from the database by name
+      #   database = Ashikawa::Core::Database.new "http://localhost:8529"
+      #   database["a"] # => #<Collection name="a">
+      # @example Get a Collection from the database by ID
+      #   database = Ashikawa::Core::Database.new "http://localhost:8529"
+      #   database["7254820"] # => #<Collection id=7254820>
       def [](collection_identifier)
         begin
           server_response = @connection.send_request "/collection/#{collection_identifier}"
@@ -55,9 +80,16 @@ module Ashikawa
         Ashikawa::Core::Collection.new self, server_response
       end
       
-      # Send a request to the connection object
+      # Sends a request to a given path (Prepends the api_string automatically)
       # 
-      # @return [String] Server Reponse
+      # @example Send a get request to the database
+      #   connection.send_request('/collection/new_collection')
+      # @example Send a post request to the database
+      #   connection.send_request('/collection/new_collection', :post => { :name => 'new_collection' })
+      # @param [String] path the path you wish to send a request to.
+      # @param [Hash] method_params additional parameters for your request. Only needed if you want to send something other than a GET request.
+      # @option method_params [Hash] :post POST data in case you want to send a POST request.
+      # @return [Hash] parsed JSON response from the server
       # @api semipublic
       def send_request(path, method_params = {})
         @connection.send_request path, method_params
