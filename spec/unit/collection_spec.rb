2f347776cbeb8f85d9dfb7cdb5f4ef77243a5e8f
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-06-11 21:33:59 +0200

    Fix indentation.

--- spec/unit/collection_spec.rb
+++ spec/unit/collection_spec.rb
@@ -9,9 +9,9 @@ describe Ashikawa::Core::Collection do
   end
   
   it "should have a name" do
-      my_collection = subject.new @database, server_response("/collections/4588")
-      my_collection.name.should == "example_1"
-    end
+    my_collection = subject.new @database, server_response("/collections/4588")
+    my_collection.name.should == "example_1"
+  end
     
   it "should accept an ID" do
     my_collection = subject.new @database, server_response("/collections/4588")
