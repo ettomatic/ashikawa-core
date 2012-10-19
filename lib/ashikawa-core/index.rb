adfe8f1cb8deb8a09cebd63679e2a134047d5bf9
moonglum <moonglum@moonbeamlabs.com>
2012-10-19 15:53:07 +0200

    Refactor: Index constructor
    * Renamed argument
    * Consistency

--- lib/ashikawa-core/index.rb
+++ lib/ashikawa-core/index.rb
@@ -25,15 +25,15 @@ module Ashikawa
       # Create a new Index
       #
       # @param [Collection] collection The collection the index is defined on
-      # @param [Hash] raw_data The JSON representation of the index
+      # @param [Hash] raw_cursor The JSON representation of the index
       # @return [Index]
       # @api
-      def initialize(collection, raw_data)
+      def initialize(collection, raw_cursor)
         @collection = collection
-        @id = raw_data["id"].split("/")[1].to_i if raw_data["id"]
-        @on = raw_data["fields"].map { |field| field.to_sym } if raw_data.has_key? "fields"
-        @type = raw_data["type"].to_sym if raw_data.has_key? "type"
-        @unique = raw_data["unique"] if raw_data.has_key? "unique"
+        @id = raw_cursor["id"].split("/")[1].to_i if raw_cursor.has_key? "id"
+        @on = raw_cursor["fields"].map { |field| field.to_sym } if raw_cursor.has_key? "fields"
+        @type = raw_cursor["type"].to_sym if raw_cursor.has_key? "type"
+        @unique = raw_cursor["unique"] if raw_cursor.has_key? "unique"
       end
 
       # Remove the index from the collection
